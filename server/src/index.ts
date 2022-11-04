e7536f35cc3f68f46796bdfe46a03620bda99854
Enrique Alpízar <ealpizr@gmail.com>
2022-11-03 18:03:34 -0700

    (server) init project

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import puppeteer from "puppeteer";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://google.com/");
+
+  res.send({
+    title: await page.title(),
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening...");
+});
