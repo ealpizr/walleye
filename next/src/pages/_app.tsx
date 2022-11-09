d47dee49203ad649c4621d878d5eb569f93968ba
Enrique Alpízar <ealpizr@gmail.com>
2022-11-09 13:09:58 -0600

    (next) home page working

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
+import Layout from "../components/Layout";
+
+import "../styles/reset.css";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Head>
+        <title>Walleye</title>
+        <link rel="icon" href="/walleye.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default MyApp;
