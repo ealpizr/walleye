b2763247d2725ca5eb30499f1947dd9ec7db3d4c
Enrique Alpízar <ealpizr@gmail.com>
2022-11-11 22:58:08 -0600

    add children information to response

--- src/pages/api/tse/id.ts
+++ src/pages/api/tse/id.ts
@@ -3,6 +3,10 @@ import { parse } from "node-html-parser";
 import type { TSEData } from "../../../types";
 import type { Override } from "../../../utils";
 import { isValidID, parseParams } from "../../../utils";
+import {
+  TSE_ID_QUERY_URL,
+  TSE_SINGLE_RESULT_URL,
+} from "../../../utils/constants";
 
 type Request = Override<
   NextApiRequest,
@@ -12,11 +16,6 @@ type Request = Override<
     };
   }
 >;
-
-const requestEndpoint =
-  "https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx";
-const resultEndpoint =
-  "https://servicioselectorales.tse.go.cr/chc/resultado_persona.aspx";
 
 const id = async (
   req: Request,
@@ -36,7 +35,7 @@ const id = async (
     });
   }
 
-  const getCookie = await fetch(requestEndpoint);
+  const getCookie = await fetch(TSE_ID_QUERY_URL);
   const sessionCookie = getCookie.headers.get("set-cookie") || "";
 
   const params = {
@@ -49,7 +48,7 @@ const id = async (
     txtcedula: query,
   };
 
-  await fetch(requestEndpoint, {
+  await fetch(TSE_ID_QUERY_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -59,12 +58,47 @@ const id = async (
     body: parseParams(params),
   });
 
-  const response = await fetch(resultEndpoint, {
+  let response = await fetch(TSE_SINGLE_RESULT_URL, {
     headers: {
       Cookie: sessionCookie,
     },
   });
   const root = parse(await response.text());
+
+  response = await fetch(TSE_SINGLE_RESULT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "User-Agent": "Mozilla/5.0", // this is required for some reason
+      Cookie: sessionCookie,
+    },
+    body: parseParams({
+      __VIEWSTATE:
+        "/wEPDwULLTE1NTg5MDEzMzIPZBYCAgUPZBYKAg0PZBYCZg9kFiYCAw8PFgQeBFRleHQFJVNPTElDSVRBUiBDRVJUSUZJQ0FDSU9OIERFIE1BVFJJTU9OSU8eB1Rvb2xUaXAFJVNPTElDSVRBUiBDRVJUSUZJQ0FDSU9OIERFIE1BVFJJTU9OSU9kZAIJDw8WAh4HVmlzaWJsZWhkZAINDw8WAh8CaGRkAhMPDxYCHwAFCTEwMjgwMDY3MmRkAhcPDxYCHwAFCjEzLzA5LzE5NDBkZAIbDw8WAh8ABRRPU0NBUiBBUklBUyAgU0FOQ0hFWmRkAh8PDxYCHwAFDUNPU1RBUlJJQ0VOU0VkZAIjDw8WAh8ABQEgZGQCJw8PFgIfAAUIODIgQcORT1NkZAIrDw8WAh8ABRFKVUFOIFJBRkFFTCBBUklBU2RkAi8PDxYCHwAFAk5PZGQCMw8PFgIfAAUBMGRkAjsPDxYCHwAFD0xJTExJQU0gU0FOQ0hFWmRkAkEPDxYCHwAFATBkZAJDDw8WAh8ABQhDQVNBRE8vQWRkAkUPDxYCHwAFATJkZAJJDw8WAh8AZWRkAk0PDxYCHwAFCU1BU0NVTElOT2RkAk8PDxYCHwBlZGQCHw8PFgIfAGVkZAInD2QWAmYPZBYCAgMPPCsAEQIBEBYAFgAWAAwUKwAAZAIpD2QWAmYPZBYCAgMPPCsAEQIBEBYAFgAWAAwUKwAAZAIrD2QWAmYPZBYCAgMPPCsAEQIBEBYAFgAWAAwUKwAAZBgEBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WBAULSW1hZ2VJbmljaW8FE0ltYWdlQ29uc3VsdGFDZWR1bGEFE0ltYWdlQ29uc3VsdGFOb21icmUFCkltYWdlU2FsaXIFDEdyaWR2b3RhY2lvbg9nZAUPR3JpZG1hdHJpbW9uaW9zD2dkBQlHcmlkaGlqb3MPZ2RqqTnz5OmfEctuMdwy1LYWtUJ0+PjMOvCe0vdnNTSsDw==",
+      __EVENTVALIDATION:
+        "/wEdABFv9odfY/ybx2DW2JE2QBf3iqbymbVNf9U++jDqKpBZeKfpigIS2tjkEVbbLpJMXRKuK+c1zGEqN0QiOtSVIjbTo8XTvMemOeiTsiCv52YIemVikE++yEdoHTLugZV7sMg9MKWwyYnyGaRupuuiPjUTpRd+w+p8QgmIF/xj83k64/we9+BwVz5Ihp+bC78EPFMCfHJxCCGkqx0cEcFZIWbctTfNpOz0CH4vKngBfzxDIS2hNtLCXCKq92TKMjYS4CX24YOX6Ab2AoRRJXYcN6RPb/5cgUWoPLK9FRe9Wehz9MYrrw85uciTs0i4KdeN/lBd3VPD6Wym8us3eHD1WYbKZrHMfDaOuX2c5DuODJSei02WSE5Vu47J1rzLM6MMWQ4uITXT3xLGIsE/l/TutLJu",
+      __ASYNCPOST: "true",
+      btnMostrarNacimiento: "Mostrar",
+    }),
+  });
+
+  const childrenRoot = parse(await response.text());
+  const children = Array.from(childrenRoot.querySelectorAll("#Gridhijos > tr"))
+    .slice(1)
+    .map((row) => {
+      return Array.from(row.childNodes)
+        .map((col) => {
+          return col.innerText;
+        })
+        .slice(2, -1);
+    })
+    .map((child) => {
+      return {
+        id: child[0],
+        name: child[2],
+        dateOfBirth: child[1],
+      };
+    });
 
   const data = {
     id: root.getElementById("lblcedula")?.innerText,
@@ -72,6 +106,15 @@ const id = async (
     dateOfBirth: root.getElementById("lblfechaNacimiento")?.innerText,
     age: root.getElementById("lbledad")?.innerText,
     deceased: root.getElementById("lbldefuncion2")?.innerText !== "",
+    father: {
+      id: root.getElementById("lblnombremadre")?.innerText || "",
+      name: root.getElementById("lblid_padre")?.innerText || "",
+    },
+    mother: {
+      id: root.getElementById("lblid_madre")?.innerText || "",
+      name: root.getElementById("lblnombremadre")?.innerText || "",
+    },
+    children: children,
   };
 
   if (!data.id) {
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,14 @@
 export const RNPDIGITAL_API_URL =
   "https://rn.rnpdigital.com/wp-admin/admin-ajax.php";
+
+export const TSE_ID_QUERY_URL =
+  "https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx";
+
+export const TSE_NAME_QUERY_URL =
+  "https://servicioselectorales.tse.go.cr/chc/consulta_nombres.aspx";
+
+export const TSE_SINGLE_RESULT_URL =
+  "https://servicioselectorales.tse.go.cr/chc/resultado_persona.aspx";
+
+export const TSE_MULTIPLE_RESULTS_URL =
+  "https://servicioselectorales.tse.go.cr/chc/muestra_nombres.aspx";
