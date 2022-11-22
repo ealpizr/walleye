6e70fe8ec7a8d6a30d5efb5526ea7843ca53e4ad
Enrique Alpízar <ealpizr@gmail.com>
2022-11-21 19:12:16 -0600

    create rnp service

---
+++ src/services/rnp.ts
@@ -0,0 +1,31 @@
+import type { ErrorResponse, RNPData } from "../types";
+import { UNKNOWN_ERROR_MESSAGE } from "../utils/constants";
+
+class RNPService {
+  static queryByID = (id: string): Promise<RNPData> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/rnp`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            query: id,
+          }),
+        });
+        const body = await response.json();
+        if (response.status !== 200) {
+          return reject(
+            (body as ErrorResponse).message || UNKNOWN_ERROR_MESSAGE
+          );
+        }
+        resolve(body as RNPData);
+      } catch (error) {
+        reject(UNKNOWN_ERROR_MESSAGE);
+      }
+    });
+  };
+}
+
+export default RNPService;
